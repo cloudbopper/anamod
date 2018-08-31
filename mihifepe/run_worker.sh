9c9769ec31e9eecd131542460cf0d8b74c189bb7
Akshay Sood <sood@cs.wisc.edu>
2018-08-31 14:14:50 -0500

    Fix condor virtualenv setup

--- mihifepe/run_worker.sh
+++ mihifepe/run_worker.sh
@@ -1,6 +1,6 @@
 # Run worker after setting up environment (typically for condor)
 
 if [ "$2" != "" ]; then
-  $HOME/.virtualenvs/$2/bin/activate_this.py
+  source $HOME/.virtualenvs/$2/bin/activate
 fi
 python3 -m mihifepe.worker $1
