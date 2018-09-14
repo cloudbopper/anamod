7dd6cbf7f6c0ebd40e1093e9363770bb1812dedc
Akshay Sood <sood@cs.wisc.edu>
2018-09-14 10:11:10 -0500

    Re-organize setup.py, add more keywords

--- setup.py
+++ setup.py
@@ -2,24 +2,17 @@
 
 from setuptools import find_packages, setup
 
+# pylint: disable = invalid-name
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
 setup(
-    name="mihifepe",
-    version="0.1dev",
-    description="Model Interpretability via Hierarchical Feature Perturbation",
-    url="https://github.com/cloudbopper/mihifepe",
     author="Akshay Sood",
-    license="MIT",
-    long_description=open("README.rst").read(),
-    packages=find_packages(),
-    python_requires=">= 3.5",
-    install_requires=[
-        "anytree~=2.4.3",
-        "h5py~=2.8.0",
-        "numpy~=1.15.0",
-        "pyhashxx~=0.1.3",
-        "scikit-learn~=0.19.2",
-        "scipy~=1.1.0"
-    ],
+    author_email='sood.iitd@gmail.com',
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Console",
@@ -30,6 +23,35 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ]
-
+    ],
+    description="Model Interpretability via Hierarchical Feature Perturbation",
+    entry_points={
+        "console_scripts": [
+            "mihifepe=mihifepe.master:main",
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+        "anytree~=2.4.3",
+        "h5py~=2.8.0",
+        "numpy~=1.15.0",
+        "pyhashxx~=0.1.3",
+        "scikit-learn~=0.19.2",
+        "scipy~=1.1.0"
+    ],
+    keywords="mihifepe",
+    license="MIT",
+    long_description=readme + "\n\n" + history,
+    name="mihifepe",
+    packages=find_packages(include=["mihifepe"]),
+    python_requires=">= 3.5",
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=[
+        "pytest",
+    ],
+    url="https://github.com/cloudbopper/mihifepe",
+    version="0.1.0dev",
+    zip_safe=False,
 )
