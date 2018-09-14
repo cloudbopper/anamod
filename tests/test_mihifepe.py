222ccf9d19c8cad49c44e6195a02aeae01c526c5
Akshay Sood <sood@cs.wisc.edu>
2018-09-13 19:28:44 -0500

    Add sample pytest

---
+++ tests/test_mihifepe.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Tests for `mihifepe` package."""
+
+import pytest
+
+@pytest.fixture
+def get_response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    None # TODO
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    None # TODO
