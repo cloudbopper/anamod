ac211c3ebe2af1235c28cec86d8f23baadc7f225
Akshay Sood <sood@cs.wisc.edu>
2018-09-14 09:56:35 -0500

    Add name/email/version to __init__.py

--- mihifepe/__init__.py
+++ mihifepe/__init__.py
@@ -1,1 +1,5 @@
-"""Model Interpretability via Hierarchical Feature Perturbation"""
+"""Top-level package for mihifepe."""
+
+__author__ = """Akshay Sood"""
+__email__ = 'sood.iitd@gmail.com'
+__version__ = '0.1.0dev'
