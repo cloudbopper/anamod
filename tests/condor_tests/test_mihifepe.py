4a61603ec437229c37caf3e49049db164030f4d4
Akshay Sood <sood@cs.wisc.edu>
2019-11-06 15:19:13 -0600

    Update condor tests to include interactions with shuffling test

--- tests/condor_tests/test_mihifepe.py
+++ tests/condor_tests/test_mihifepe.py
@@ -134,3 +134,21 @@ def test_condor_simulation_noisy_interactions(file_regression, tmpdir):
     with open(fdr_filename, "r") as fdr_file:
         fdr = sorted(fdr_file.readlines())
     file_regression.check("\n".join(fdr), extension="_fdr.json")
+
+
+def test_condor_simulation_shuffling_interactions(file_regression, tmpdir):
+    """Test simulation with interactions and shuffling perturbations"""
+    func_name = sys._getframe().f_code.co_name
+    output_dir = "%s/output_dir_%s" % (tmpdir, func_name)
+    pvalues_filename = "%s/%s" % (output_dir, constants.INTERACTIONS_PVALUES_FILENAME)
+    cmd = ("python -m mihifepe.simulation -condor -seed 5 -num_instances 1000 -num_features 10 -fraction_relevant_features 0.5"
+           " -analyze_interactions -hierarchy_type random -perturbation shuffling -num_shuffling_trials 200 -noise_multiplier 0.0 -noise_type additive_gaussian"
+           " -num_interactions 3 -output_dir %s" % output_dir)
+    subprocess.check_call(cmd, shell=True)
+    with open(pvalues_filename, "r") as pvalues_file:
+        pvalues = sorted(pvalues_file.readlines())
+    file_regression.check("\n".join(pvalues), extension="_pvalues.csv")
+    fdr_filename = "%s/%s/%s.csv" % (output_dir, constants.INTERACTIONS_FDR_DIR, constants.HIERARCHICAL_FDR_OUTPUTS)
+    with open(fdr_filename, "r") as fdr_file:
+        fdr = sorted(fdr_file.readlines())
+    file_regression.check("\n".join(fdr), extension="_fdr.json")
