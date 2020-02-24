814aa51ad97d890d51e9921e64e059b27dffc356
Akshay Sood <sood.iitd@gmail.com>
2020-02-24 19:11:23 +0530

    Add condor tests/test generation for temporal model analysis

--- tests/gen_condor_tests.py
+++ tests/gen_condor_tests.py
@@ -4,9 +4,10 @@ import argparse
 import os
 import re
 import shutil
+from anamod.constants import HIERARCHICAL, TEMPORAL
 
-TEST_FILENAME = "test_hierarchical.py"
-GOLD_DIRECTORY = "test_hierarchical"
+TESTS = {HIERARCHICAL: "test_hierarchical.py", TEMPORAL: "test_temporal.py"}
+GOLDS = {HIERARCHICAL: "test_hierarchical", TEMPORAL: "test_temporal"}
 CONDOR_TEST_DIRECTORY = "condor_tests"
 TEST_SIMULATION = "test_simulation"
 TEST_CONDOR_SIMULATION = "test_condor_simulation"
@@ -18,31 +19,32 @@ def main():
     # Parse args
     parser = argparse.ArgumentParser()
     parser.add_argument("-overwrite_golds", help="overwrite existing gold files", action="store_true")
-    parser.add_argument("-test_filename", default=TEST_FILENAME)
-    parser.add_argument("-gold_directory", default=GOLD_DIRECTORY)
+    parser.add_argument("-type", default=HIERARCHICAL, choices=[HIERARCHICAL, TEMPORAL])
     args = parser.parse_args()
     test_dir = os.path.dirname(os.path.realpath(__file__))
     condor_test_dir = os.path.join(test_dir, CONDOR_TEST_DIRECTORY)
     if not os.path.exists(condor_test_dir):
         os.makedirs(condor_test_dir)
+    test_filename = TESTS[args.type]
+    gold_dir = GOLDS[args.type]
     # Write test file
-    test_filename = os.path.join(test_dir, TEST_FILENAME)
-    condor_test_filename = os.path.join(test_dir, CONDOR_TEST_DIRECTORY, TEST_FILENAME)
+    condor_test_filename = os.path.join(test_dir, CONDOR_TEST_DIRECTORY, test_filename)
     with open(condor_test_filename, "w") as condor_test_file:
-        with open(test_filename, "r") as test_file:
+        test_filename_abs = os.path.join(test_dir, test_filename)
+        with open(test_filename_abs, "r") as test_file:
             for line in test_file:
                 for pattern, replacement in SUBSTITUTIONS.items():
                     line = re.sub(pattern, replacement, line)
                 condor_test_file.write(line)
     # Write gold files
-    gold_dir = os.path.join(test_dir, GOLD_DIRECTORY)
-    condor_gold_dir = os.path.join(condor_test_dir, GOLD_DIRECTORY)
+    condor_gold_dir = os.path.join(condor_test_dir, gold_dir)
     if not os.path.exists(condor_gold_dir):
         os.makedirs(condor_gold_dir)
-    for gold_filename in os.listdir(gold_dir):
+    gold_dir_abs = os.path.join(test_dir, gold_dir)
+    for gold_filename in os.listdir(gold_dir_abs):
         condor_gold_filename = os.path.join(condor_gold_dir, re.sub(TEST_SIMULATION, TEST_CONDOR_SIMULATION, gold_filename))
         if not os.path.exists(condor_gold_filename) or args.overwrite_golds:
-            shutil.copyfile(os.path.join(gold_dir, gold_filename), condor_gold_filename)
+            shutil.copyfile(os.path.join(gold_dir_abs, gold_filename), condor_gold_filename)
 
 
 if __name__ == "__main__":
