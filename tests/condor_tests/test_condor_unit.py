2059e48508983cb371b3986b0696997f97696ccb
Akshay Sood <sood.iitd@gmail.com>
2020-04-02 20:03:13 -0500

    Condor: move to poll-based tracking; update unit test to run 100 jobs; retry jobs on unknown failures, upto limit

--- tests/condor_tests/test_condor_unit.py
+++ tests/condor_tests/test_condor_unit.py
@@ -1,28 +1,25 @@
-"""Test condor on non-shared filesystem"""
+"""Test condor functionality"""
 
 import os
 from anamod.utils import CondorJobWrapper
-
-# TODO: Test cleanup
 
 
 # pylint: disable = protected-access, too-many-locals
 def test_condor_cat(tmpdir, shared_fs):
     """Test condor functionality"""
-    files = [f"{tmpdir}/file0.txt", f"{tmpdir}/file1.txt"]
-    with open(files[0], "w") as file0:
-        file0.write("Hello")
-    with open(files[1], "w") as file1:
-        file1.write("World")
-    dirs = [f"{tmpdir}/dir0", f"{tmpdir}/dir1"]
-    if shared_fs:
-        cmds = [f"cat {os.path.abspath(files[idx])} > {os.path.abspath(dirs[idx])}/newfile.txt" for idx in range(2)]
-    else:
-        cmds = [f"cat {os.path.basename(files[idx])} > {os.path.basename(dirs[idx])}/newfile.txt" for idx in range(2)]
-    jobs = [None] * 2
-    for idx, cmd in enumerate(cmds):
-        directory = dirs[idx]
-        job = CondorJobWrapper(cmd, [files[idx]], directory, shared_filesystem=shared_fs)
+    num_jobs = 100
+    dirs = [None] * num_jobs
+    jobs = [None] * num_jobs
+    for idx in range(num_jobs):
+        filename = f"{tmpdir}/file{idx}.txt"
+        with open(filename, "w") as filep:
+            filep.write(f"{idx}")
+        dirs[idx] = f"{tmpdir}/dir{idx}"
+        if shared_fs:
+            cmd = f"cat {os.path.abspath(filename)} > {os.path.abspath(dirs[idx])}/newfile.txt"
+        else:
+            cmd = f"cat {os.path.basename(filename)} > {os.path.basename(dirs[idx])}/newfile.txt"
+        job = CondorJobWrapper(cmd, [filename], dirs[idx], shared_filesystem=shared_fs)
         job.run()
         jobs[idx] = job
     CondorJobWrapper.monitor(jobs)
@@ -30,4 +27,4 @@ def test_condor_cat(tmpdir, shared_fs):
     for directory in dirs:
         with open(f"{directory}/newfile.txt", "r") as newfile:
             output += newfile.read()
-    assert output == "HelloWorld"
+    assert output == "".join([f"{idx}" for idx in range(num_jobs)])
