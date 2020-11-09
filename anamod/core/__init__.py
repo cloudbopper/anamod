9cc1d5c87145248460df0b7e1fb6c03294d846c7
Akshay Sood <sood.iitd@gmail.com>
2020-11-09 16:18:41 +0530

    Move core files to separate folder (core)

---
+++ anamod/core/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for anamod."""
+
+__author__ = """Akshay Sood"""
+__email__ = "sood.iitd@gmail.com"
+__version__ = "0.1.0"
+
+
+from anamod.model_analyzer import ModelAnalyzer  # noqa: F401
